7630364c1845261daa7c216962b830df54d94946
Luke Anger <112991266+LukeAnger@users.noreply.github.com>
2024-07-02 09:32:10 -0400

    removed unnecessary code

--- query.py
+++ query.py
@@ -1,4 +1,3 @@
-import argparse
 from langchain_community.vectorstores import Chroma
 from langchain.prompts import ChatPromptTemplate
 from langchain_community.llms.ollama import Ollama
@@ -17,39 +16,38 @@ Answer the question based only on the following context:
 Answer the question based on the above context {question} and provide a detailed explanation.
 """
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('question', type=str, help='The question')
-    args = parser.parse_args()
-    question = args.question
-    query_rag(question)
+## Implement some way to have a persistent chatbot that can answer questions based on a context and a question
 
-
-def query_rag(question):
+def reset_vector_db():
     db = Chroma(persist_directory=CHROMA_PATH, embedding_function=get_embeddings())
+    db.reset()
 
+def main():
+    db = Chroma(persist_directory=CHROMA_PATH, embedding_function=get_embeddings())
     model = Ollama(model='llama3')
-
-    sim_search = db.similarity_search_with_score(question, k=25)
-    print("Similarity search results:", sim_search)
-
-    # context = sim_search[0][0].page_content
-    # concat first 5 of sim search
-    context = ''
-    for i in range(0, 5):
-        context += sim_search[i][0].page_content + ' '
     
+    while True:
+        question = input("Enter your question: ")
+        sim_search = db.similarity_search_with_score(question, k=25)
+        # print("Similarity search results:", sim_search)
 
-    print("---------------------------------------------------------------------------------------------------------------------------------------------------------")
+        # context = sim_search[0][0].page_content
+        # concat first 5 of sim search
+        context = ''
+        for i in range(0, 5):
+            context += sim_search[i][0].page_content + ' '
+        
 
-    print("Context:", context)
+        # print("---------------------------------------------------------------------------------------------------------------------------------------------------------")
 
-    prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
-    prompt = prompt_template.format(context=context, question=question)
+        # print("Context:", context)
 
-    response = model.invoke(prompt)
-    print("Response:", response)
-    return response
+        prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
+        prompt = prompt_template.format(context=context, question=question)
+
+        response = model.invoke(prompt)
+        print("Response:", response)
+        return response
 
 if __name__ == '__main__':
     main()
