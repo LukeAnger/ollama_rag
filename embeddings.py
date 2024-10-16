16b3034d66077c40659a0a0e3f0ba2a4b7ab106b
Luke Anger <112991266+LukeAnger@users.noreply.github.com>
2024-10-16 10:19:32 -0400

    updated the embeddings func and req.txt

--- embeddings.py
+++ embeddings.py
@@ -1,26 +1,6 @@
-# from langchain_community.embeddings import OllamaEmbeddings
-
-# import boto3
-# from langchain_community.embeddings.bedrock import BedrockEmbeddings
-
-# from langchain_nomic.embeddings import NomicEmbeddings
-
-from langchain_huggingface  import HuggingFaceEmbeddings
+from langchain.embeddings  import HuggingFaceEmbeddings
 
 def get_embeddings():
-    # embeddings = OllamaEmbeddings(model='llama3')
-
-    # bedrock_client = boto3.client(
-    #     service_name='bedrock_runtime', 
-    #     region_name='us-east-1'
-    #     )
-    # embeddings = BedrockEmbeddings(
-    #     credentials_profile_name='default',
-    #     region_name='us-east-1',
-    #     )
-    
-    # embeddings = NomicEmbeddings(model="nomic-embed-text-v1.5")
-
     embeddings = HuggingFaceEmbeddings()
 
     return embeddings
